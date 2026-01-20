bd8498d5390ff7549e5222ed1ae789759c2871f3
DennyLei <leidenny1@gmail.com>
2026-01-20 19:11:07 +0000

    qqqq

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -5,7 +5,7 @@ public class PigLatin {
         //precondition: sWord is a valid String of length greater than 0.
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
 	      for(int i=0;i<sWord.length();i++){
-             if(sWord.substring(i,i+1).equals("a") || sWord.substring(i,i+1).equals("e"))return i;
+            if(sWord.substring(i,i+1).equals("a") || sWord.substring(i,i+1).equals("e"))return i;
             else if(sWord.substring(i,i+1).equals("i") || sWord.substring(i,i+1).equals("o"))return i;
             else if(sWord.substring(i,i+1).equals("u"))return i;}
         return -1;
